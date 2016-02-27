44d137b360ed38db7fcf37bc74816704e052769f
Patrick Williams <pwmckenna@gmail.com>
2016-02-27 15:15:35 -0800

    reply

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ const app = express();
 app.post('/sms', json(), urlencoded(), text(), (req, res, next) => {
   console.log('received an sms', req.body.Body);
   reply(req.body.Body).then(msg => (
-    res.status(200).send(`<Response><Sms>${msg}</Sms></Response>`);
+    res.status(200).send(`<Response><Sms>${msg}</Sms></Response>`)
   ), err => (
-    res.status(200).send(`<Response><Sms>${err.message}</Sms></Response>`);
+    res.status(200).send(`<Response><Sms>${err.message}</Sms></Response>`)
   ))
 });
 
