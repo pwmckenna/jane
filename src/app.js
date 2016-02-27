98ee831311164c03cc4e8c8deef7604a554749f7
Patrick Williams <pwmckenna@gmail.com>
2016-02-27 15:11:57 -0800

    body

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import { json, urlencoded, text } from 'body-parser';
 const app = express();
 
 app.post('/sms', json(), urlencoded(), text(), (req, res, next) => {
-  console.log('received an sms', req);
+  console.log('received an sms', req.body.Body);
 
   const msg = 'Roger that';
   res.status(200).send(`<Response><Sms>${msg}</Sms></Response>`);
