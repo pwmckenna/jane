ec692290d43f169dcba2099ed2e6692bdb472d59
Patrick Williams <pwmckenna@gmail.com>
2016-02-27 14:57:33 -0800

    server vs index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 require('babel-core/register');
 require('./config');
-require('./jane');
+require('./app').listen(process.env.PORT, () => (
+  console.log(`listening on port ${process.env.PORT}`)
+));
